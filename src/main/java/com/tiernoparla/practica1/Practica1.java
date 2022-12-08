7660d0c1fef53d579011b609c1ce06c725d0041c
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-08 10:42:00 +0100

    limpieza final. Acabado

--- src/main/java/com/tiernoparla/practica1/Practica1.java
+++ src/main/java/com/tiernoparla/practica1/Practica1.java
@@ -1,42 +1,41 @@
 package com.tiernoparla.practica1;
 
 import static java.lang.Math.*;
-
 import java.util.Arrays;
 import java.util.Random;
 
 public class Practica1 {
-    /**
-     *
-     */
 
     public static void main(String[] args) {
-        
-        final int TAMANO = 2;
-        final byte[] mensaje = crearMensaje(TAMANO);
 
+        // generar mensaje
+        final int TAMANO = 10;
+        final byte[] MENSAJE = crearMensaje(TAMANO);
 
-        /* 
         // sender
-        byte[] codigoHamming = calcularCodigoHamming(mensaje);
+        final byte[] CODIGO_HAMMING = calcularCodigoHamming(MENSAJE);
 
         // noise
-        byte[] mensajeRecibido = generarCambiosAleatorios(codigoHamming);
+        final byte[] MENSAJE_RECIBIDO = generarCambiosAleatorios(CODIGO_HAMMING);
 
         // reciever
-        concluirSiHayErrores(mensajeRecibido, recalcularMensaje(mensajeRecibido));
-        */
+        concluirSiHayErrores(MENSAJE_RECIBIDO, recalcularMensaje(MENSAJE_RECIBIDO));
 
     } // main
 
     private static byte[] crearMensaje(final int TAMANO) {
+
         final int DOS = 2;
+        final byte[] MENSAJE = new byte[TAMANO];
+
         Random rnd = new Random();
-        final byte[] mensaje = new byte[TAMANO];
-        for(int i = 0; i < TAMANO; i++){
-            mensaje[i] = (byte) rnd.nextInt(DOS);
+
+        for (int i = 0; i < TAMANO; i++) {
+            MENSAJE[i] = (byte) rnd.nextInt(DOS);
         }
-        return mensaje;
+
+        return MENSAJE;
+
     }
 
     public static byte[] generarCambiosAleatorios(byte[] codigoHamming) {
@@ -45,62 +44,68 @@ public class Practica1 {
         final byte UNO = 1;
         final byte DOS = 2;
 
-        Random rnd = new Random();
         byte[] mensajeRecibido = codigoHamming.clone();
+
+        Random rnd = new Random();
         final byte NUMERO_CAMBIOS = (byte) rnd.nextInt(3);
 
         switch (NUMERO_CAMBIOS) {
             case CERO:
                 break;
             case UNO:
-                // hacer 1 cambio
                 mensajeRecibido = hacerUnCambio(mensajeRecibido);
                 break;
             case DOS:
-                // hacer 2 cambios
                 mensajeRecibido = hacerDosCambios(codigoHamming, mensajeRecibido);
                 break;
         }
+
         return mensajeRecibido;
     }
 
     public static byte[] hacerDosCambios(byte[] codigoHamming, byte[] mensajeRecibido) {
+
         while (Arrays.equals(codigoHamming, mensajeRecibido)) {
             mensajeRecibido = hacerUnCambio(mensajeRecibido);
             mensajeRecibido = hacerUnCambio(mensajeRecibido);
         }
+
         return mensajeRecibido;
     }
 
     public static byte[] hacerUnCambio(byte[] mensajeRecibido) {
+
         Random rnd = new Random();
         final int POSICION_CAMBIO = rnd.nextInt(mensajeRecibido.length);
+
         mensajeRecibido[POSICION_CAMBIO] = (byte) (1 - mensajeRecibido[POSICION_CAMBIO]);
-        System.out.println("pos cambio: " + POSICION_CAMBIO);
+
         return mensajeRecibido;
     }
 
     public static byte[] recalcularMensaje(final byte[] mensajeRecibido) {
-        // hacemos el reciever
-        final byte[] mensajeDeberiaSer = mensajeRecibido.clone();
-        // recalculamos bits de paridad
-        mensajeDeberiaSer[0] = calcularBitParidad(mensajeDeberiaSer, 0);
-        for (int i = 0; pow(2, i) < mensajeDeberiaSer.length; i++) {
-            mensajeDeberiaSer[(int) pow(2, i)] = calcularBitParidad(mensajeDeberiaSer, (int) pow(2, i));
+
+        final byte[] MENSAJE_DEBERIA_SER = mensajeRecibido.clone();
+
+        MENSAJE_DEBERIA_SER[0] = calcularBitParidad(MENSAJE_DEBERIA_SER, 0);
+
+        for (int i = 0; pow(2, i) < MENSAJE_DEBERIA_SER.length; i++) {
+            final int POSICION = (int) pow(2, i);
+            MENSAJE_DEBERIA_SER[POSICION] = calcularBitParidad(MENSAJE_DEBERIA_SER, POSICION);
         }
-        return mensajeDeberiaSer;
+
+        return MENSAJE_DEBERIA_SER;
     }
 
     public static void concluirSiHayErrores(final byte[] mensajeRecibido, final byte[] mensajeDeberiaSer) {
-        // detectamos si hay algún error
+
         boolean hayError = !Arrays.equals(mensajeDeberiaSer, mensajeRecibido);
 
         final byte CERO = 0;
-        // concluimos si hay 0, 1 o 2 errores
+
         if (!hayError) {
             System.out.println("Mensaje recibido sin errores.");
         } else {
-            // vemos si hay 1 error
             if (mensajeDeberiaSer[CERO] != mensajeRecibido[CERO]) {
                 System.out.println("Mensaje recibido con un error en: " + detectarError(mensajeRecibido));
             } else {
@@ -110,13 +115,15 @@ public class Practica1 {
     }
 
     public static int detectarError(final byte[] mensajeRecibido) {
-        // detectar el error
+
         int posicionError = 0;
+
         for (int i = 0; i < mensajeRecibido.length; i++) {
             if (mensajeRecibido[i] != 0) {
                 posicionError = (posicionError ^ i);
             }
         }
+
         return posicionError;
     }
 
@@ -125,6 +132,7 @@ public class Practica1 {
         byte[] codigoHamming = new byte[calculcarNumeroBitsParidad(mensaje) + mensaje.length + 1];
 
         int indiceMensaje = mensaje.length - 1;
+
         for (int i = codigoHamming.length - 1; i >= 0; i--) {
             if (esPotenciaDe2(i) || i == 0) {
                 codigoHamming[i] = calcularBitParidad(codigoHamming, i);
@@ -133,24 +141,31 @@ public class Practica1 {
                 indiceMensaje--;
             }
         }
+
         return codigoHamming;
     }
 
     public static byte calcularBitParidad(byte[] codigoHamming, int posicion) {
+
         int counter = 0;
+
         for (int i = codigoHamming.length - 1; i > posicion; i--) {
             if ((posicion & i) == posicion) {
                 counter += codigoHamming[i];
             }
         }
+
         return (byte) (counter % 2);
     }
 
     public static int calculcarNumeroBitsParidad(final byte[] mensaje) {
+
         int numeroBitsParidad = 0;
+
         while (pow(2, numeroBitsParidad) - (numeroBitsParidad + 1) < mensaje.length) {
             numeroBitsParidad++;
         }
+
         return numeroBitsParidad;
     } // caclulcarNumeroBitsParidad
 
