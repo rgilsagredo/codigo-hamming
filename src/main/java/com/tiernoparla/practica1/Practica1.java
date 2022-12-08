47c8440f2ee3251789c73a80481701df21953089
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-08 07:49:46 +0100

    añadido metod que calcula número de bist de paridad
    Creo un método que calcula, dado un mensaje y usando su tamaño,
    cuántos bits de paridad son necesarios para el código de hamming.
    Luego hay que añadir manualmente el bit de paridad global.

--- src/main/java/com/tiernoparla/practica1/Practica1.java
+++ src/main/java/com/tiernoparla/practica1/Practica1.java
@@ -1,5 +1,6 @@
 package com.tiernoparla.practica1;
 
+import static java.lang.Math.*;
 
 public class Practica1 
 {
@@ -10,5 +11,20 @@ public class Practica1
         final byte[] mensaje = {0,1,0,0,1,1,0,1,0,1,0};
         final byte[] codigoHamming = {0,0,0,0,1,1,0,0,0,1,1,0,1,0,1,0};
 
+        // sender
+        // calcular num bit paridad
+        int numeroBitsParidad = calculcarNumeroBitsParidad(mensaje);
+
+        System.out.println(numeroBitsParidad);
+
     } // main
+
+    public static int calculcarNumeroBitsParidad(final byte[] mensaje) {
+        int numeroBitsParidad = 0;
+        while(pow(2,numeroBitsParidad) - (numeroBitsParidad + 1) < mensaje.length){
+            numeroBitsParidad++;
+        }
+        return numeroBitsParidad;
+    } // caclulcarNumeroBitsParidad
+
 } // Practica1
