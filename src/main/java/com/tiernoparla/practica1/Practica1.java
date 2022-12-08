78bdf27f6ed06f62414170fff704e408c69bac9a
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-08 07:21:52 +0100

    commit inicial

---
+++ src/main/java/com/tiernoparla/practica1/Practica1.java
@@ -0,0 +1,13 @@
+package com.tiernoparla.practica1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
